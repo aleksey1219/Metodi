d29027e33223fd3c60ba6726caf5d6ecb37c87a6
DNS <glotov-aleks@mail.ru>
2023-05-12 00:17:06 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    public static void examination(int proverka) {
+        if (proverka % 4 == 0 && proverka % 100 != 0 || proverka % 400 == 0) {
+            System.out.println(proverka + " год является високосным");
+        } else {
+            System.out.println(proverka + " год является не високосным");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        examination(2019);
     }
 }

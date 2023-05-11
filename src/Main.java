e518fe3e41b3ac3d49945ac805359e17b366de6d
DNS <glotov-aleks@mail.ru>
2023-05-12 01:09:26 +0500

    task3

--- src/Main.java
+++ src/Main.java
@@ -21,14 +21,34 @@ public class Main {
         }
     }
 
+    public static void deliver(int km) {
+        int day = 1;
+        if (km < 20) {
+            System.out.println("Потребуется дней: " + day);
+        } else if (km >= 20 && km < 60) {
+            System.out.println("Потребуется дней: " + (day + 1));
+        } else if (km >= 60 && km < 100) {
+            System.out.println("Потребуется дней: " + (day + 2));
+        } else if (km >= 100) {
+            System.out.println("Доставки нет");
+        }
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Решение задачи №1");
         examination(2019);
         task2();
+        task3();
     }
+
     public static void task2() {
         System.out.println("Решение задачи №2");
-       install(1,2015);
+        install(1, 2015);
+    }
+
+    public static void task3() {
+        System.out.println("Решение задачи №3");
+        deliver(2);
     }
 }
